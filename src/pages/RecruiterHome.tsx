5d83fdd231fb602e5c43f11ac4c2fc6f534fbed1
Aidan Robinson <120530116+arob12inson@users.noreply.github.com>
2024-12-29 18:57:08 -0800

    quick formatting change

--- src/pages/RecruiterHome.tsx
+++ src/pages/RecruiterHome.tsx
@@ -28,9 +28,9 @@ const JobsTable = ({ jobs }: { jobs: Job[] }) => {
             <table className="min-w-full divide-y divide-gray-200">
             <thead className="bg-gray-100 rounded-t-lg sticky top-0">
                 <tr>
-                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Title</th>
-                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Date Posted</th>
-                <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Status</th>
+                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Title</th>
+                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Date Posted</th>
+                    <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Status</th>
                 </tr>
             </thead>
             <tbody className="bg-white divide-y divide-gray-200">
